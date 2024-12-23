fffce459bfa9302c00fc8fc732481f387372dfab
Rohit Kumar <125601198+rohitkumar3848@users.noreply.github.com>
2024-12-23 12:02:34 +0530

    Add files via upload

---
+++ Score.js
@@ -0,0 +1,27 @@
+let Globals = require('Globals');
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        scoreLabel: {
+            default: null,
+            type: cc.Label
+        }
+    },
+
+    // onLoad() {
+    //     // Display the score when the Score scene is loaded
+
+    // },
+
+    start() {
+        // Additional logic for the score screen if needed
+        this.scoreLabel.string = "Score:" + Globals.score.toString();
+        this.node.on('touchend', this.onTapToPlayAgain, this);
+    },
+
+    onTapToPlayAgain() {
+        cc.director.loadScene('Game');
+    }
+});
